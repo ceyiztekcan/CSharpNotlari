f38cf2996c9415bb26e0eed714b9f98399a55bd7
ceyiz.tekcan <ceyiz.tekcan@ALS-YG39104-J02>
2016-12-05 11:56:32 +0200

    devam et

--- Array/Array/Program.cs
+++ Array/Array/Program.cs
@@ -21,29 +21,74 @@ namespace Array
             //}
             //Console.ReadKey();
 
-            Console.Write("sınıf mevcudu ?:");
-            int mevcut = Convert.ToInt32(Console.ReadLine());
-            string[] ogrenciler = new string[mevcut];
+            //Console.Write("sınıf mevcudu ?:");
+            //int mevcut = Convert.ToInt32(Console.ReadLine());
+            //string[] ogrenciler = new string[mevcut];
 
-            for (int i = 0; i < ogrenciler.Length; i++)
+            //for (int i = 0; i < ogrenciler.Length; i++)
+            //{
+            //    Console.Write("{0}. öğrencini ismi: ",i);
+            //    ogrenciler[i] = Console.ReadLine();
+
+            //}
+
+            //Console.Clear();
+
+            //Console.WriteLine("öğrenciler : \n");
+
+            //foreach (var item in ogrenciler)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.ReadKey();
+
+            //Random random = new Random();
+            //int pozitif = 0, negatif =0, sifir = 0;
+
+            //Console.WriteLine("kaç sayı olsun :?");
+            //int boyut = Convert.ToInt32(Console.ReadLine());
+            //int [] array = new int[boyut];
+            //Console.Clear();
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = random.Next(-100, 101);
+
+
+            //}
+
+            //foreach (var sayi in array)
+            //{
+            //    Console.WriteLine(sayi);
+            //    if (sayi>0)
+            //    {
+            //        pozitif++;
+
+            //    }
+            //    else if (sayi<0)
+            //    {
+            //        negatif++;
+            //    }
+            //    else
+            //    {
+            //        sifir++;
+            //    }
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("{0} tane pozitif var ", pozitif);
+            //Console.WriteLine("{0} tane negatif var ", negatif);
+            //Console.WriteLine("{0} tane sıfır var ", sifir);
+
+            string[] gunler = new string[] { "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar" };
+
+            Console.WriteLine("Haftanın kaçıncı günü :?");
+            int sira = Convert.ToInt16(Console.ReadLine());
+
+            foreach (var gun in gunler)
             {
-                Console.Write("{0}. öğrencini ismi: ",i);
-                ogrenciler[i] = Console.ReadLine();
-
+                Console.WriteLine(gun);
             }
+           
 
-            Console.Clear();
-
-            Console.WriteLine("öğrenciler : \n");
-
-            foreach (var item in ogrenciler)
-            {
-                Console.WriteLine(item);
-            }
-            Console.ReadKey();
-
-
-            
         }
     }
 }
