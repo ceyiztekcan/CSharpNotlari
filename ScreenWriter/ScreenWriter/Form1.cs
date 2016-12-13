75dc864a44dd4b5a80c5edab5f8b79c34d4035e4
ceyiz.tekcan <ceyiz.tekcan@ALS-YG39104-J02>
2016-12-13 11:39:53 +0200

    otobus

---
+++ ScreenWriter/ScreenWriter/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenWriter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> isimler = new List<string>() { "Esranur", "Çeyiz", "Halil", "Bengisu", "Hakan", "Yunus", "Yankı", "Atakan", "OzanK", "OzanA ", "Aytekin", "İrfan", "Cemal", "Merve" };
+        Random rnd = new Random();
+        int sayac = 0;
+
+        private Color Rasgelerenk()
+        {
+            Color color;
+            int red = rnd.Next(0, 256);
+            int green = rnd.Next(0, 256);
+            int blue = rnd.Next(0, 256);
+            color = Color.FromArgb(red, green, blue);
+            return color;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            this.WindowState = FormWindowState.Maximized;
+          
+          
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            int index = rnd.Next(0, 14);
+            string isim = isimler[index];
+            int fontSize = rnd.Next(20, 40);
+            Graphics g = CreateGraphics();
+            Color color = Rasgelerenk();
+            int x = rnd.Next(10, this.Width - 50);
+            int y = rnd.Next(10, this.Height - 50);
+            g.DrawString(isim, new Font(FontFamily.GenericSansSerif, fontSize, FontStyle.Bold), new SolidBrush(color), x, y);
+
+            if (sayac == 200)
+            {
+                this.BackColor = Rasgelerenk();
+                this.Invalidate();
+                
+                sayac = 0;
+            }
+            sayac++;
+        }
+        
+    }
+    
+}
---
+++ otobus/otobus/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace otobus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }int sayac =1;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Button btn = new Button();
+                    btn.Width = 30;
+                    btn.Height = 30;
+                    btn.Left = btn.Width * j;
+                    btn.Top = btn.Height*i;
+                    btn.Text = sayac.ToString();
+                    this.Controls.Add(btn);
+                    sayac++;
+                }
+               
+            }
+            
+        }
+    }
+}
