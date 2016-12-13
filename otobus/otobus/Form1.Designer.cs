89891c17e1f7f4c3191a899df106b1338f6d3913
ceyiz.tekcan <ceyiz.tekcan@ALS-YG39104-J02>
2016-12-13 12:00:47 +0200

    update

--- otobus/otobus/Form1.Designer.cs
+++ otobus/otobus/Form1.Designer.cs
@@ -43,7 +43,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(391, 423);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
--- otobus/otobus/Form1.cs
+++ otobus/otobus/Form1.cs
@@ -19,18 +19,27 @@ namespace otobus
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            for (int i = 0; i < 12; i++)
+            for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 5; j++)
                 {
-                    Button btn = new Button();
-                    btn.Width = 30;
-                    btn.Height = 30;
-                    btn.Left = btn.Width * j;
-                    btn.Top = btn.Height*i;
-                    btn.Text = sayac.ToString();
-                    this.Controls.Add(btn);
-                    sayac++;
+
+                   
+
+                if ((j!=2 || i==9) && (i!=5 || j<2))
+                    {
+                        Button btn = new Button();
+                        btn.Width = 30;
+                        btn.Height = 30;
+                        btn.Left = btn.Width * j;
+                        btn.Top = btn.Height * i;
+                        btn.Text = sayac.ToString();
+                        this.Controls.Add(btn);
+                        sayac++;
+
+                    }
+                   
+                    
                 }
                
             }
