fd5c69ab1ce1269789ca69c7aea077d0aca291a5
ceyiz.tekcan <ceyiz.tekcan@ALS-YG39104-J02>
2016-12-13 09:28:44 +0200

    1

---
+++ Gecisler/Gecisler/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Gecisler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtBox1 = new System.Windows.Forms.TextBox();
+            this.BtnGonder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtBox1
+            // 
+            this.TxtBox1.Location = new System.Drawing.Point(81, 82);
+            this.TxtBox1.Name = "TxtBox1";
+            this.TxtBox1.Size = new System.Drawing.Size(100, 20);
+            this.TxtBox1.TabIndex = 0;
+            // 
+            // BtnGonder
+            // 
+            this.BtnGonder.Location = new System.Drawing.Point(94, 117);
+            this.BtnGonder.Name = "BtnGonder";
+            this.BtnGonder.Size = new System.Drawing.Size(75, 23);
+            this.BtnGonder.TabIndex = 1;
+            this.BtnGonder.Text = "Gönder";
+            this.BtnGonder.UseVisualStyleBackColor = true;
+            this.BtnGonder.Click += new System.EventHandler(this.BtnGonder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.BtnGonder);
+            this.Controls.Add(this.TxtBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+           
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtBox1;
+        private System.Windows.Forms.Button BtnGonder;
+    }
+}
+
---
+++ Gecisler/Gecisler/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gecisler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnGonder_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();//form objesi oluşturduk
+            form.mesaj = TxtBox1.Text;
+            form.Show();
+            this.Hide();
+        }
+    }
+}
