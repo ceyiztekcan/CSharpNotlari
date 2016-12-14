e54e0915b522d1c2c5a0aac7bc216044cbd1c3a5
çeyiz tekcan <ceyiztekcan@hotmail.com>
2016-12-14 06:01:02 +0200

    ev

--- otobus/otobus/Form1.cs
+++ otobus/otobus/Form1.cs
@@ -15,35 +15,95 @@ namespace otobus
         public Form1()
         {
             InitializeComponent();
-        }int sayac =1;
-
+        }
+       public int sayac = 1;
+        public List<int> koltukNo = new List<int>();
         private void Form1_Load(object sender, EventArgs e)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
 
-                   
+        }
+        private void Btn_click(object sender, EventArgs e)
+        {
 
-                if ((j!=2 || i==9) && (i!=5 || j<2))
-                    {
-                        Button btn = new Button();
-                        btn.Width = 30;
-                        btn.Height = 30;
-                        btn.Left = btn.Width * j;
-                        btn.Top = btn.Height * i;
-                        btn.Text = sayac.ToString();
-                        this.Controls.Add(btn);
-                        sayac++;
-
-                    }
-                   
-                    
-                }
-               
-            }
+            Button btn = sender as Button;
+            btn.BackColor = Color.Red;
+            btn.Text = sayac.ToString();
+            Form2 form2 = new Form2();
+            form2.kayit = sayac.ToString();
+            form2.Show();
             
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+
+            if (comboBox1.SelectedItem.ToString() == "Otobus_1")
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+
+
+
+                        if ((j != 2 || i == 9) && (i != 5 || j < 2))
+                        {
+                            Button btn = new Button();
+                            btn.Width = 50;
+                            btn.Height = 50;
+                            btn.Left = btn.Width * j;
+                            btn.Top = btn.Height * i;
+                            btn.Text = sayac.ToString();
+                            
+                            this.Controls.Add(btn);
+                            koltukNo.Add(sayac);
+                            btn.Click += Btn_click;
+
+                            sayac++;
+
+                        }
+
+
+                    }
+
+                }
+            }
+
+            if (comboBox1.SelectedItem.ToString() == "Otobus_2")
+            {
+                
+
+                for (int i = 0; i < 12; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+
+
+
+                        if ((j != 2 || i == 11) && (i != 6 || j < 2))
+                        {
+                            Button btn = new Button();
+                            btn.Width = 50;
+                            btn.Height = 50;
+                            btn.Left = btn.Width * j;
+                            btn.Top = btn.Height * i;
+                            btn.Text = sayac.ToString();
+                            this.Controls.Add(btn);
+                            koltukNo.Add(sayac);
+                            btn.Click += Btn_click;
+                            sayac++;
+
+                        }
+
+
+                    }
+
+                }
+
+            }
+
+        }
+
     }
 }
---
+++ otobus/otobus/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace otobus
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            
+        }
+       public List<string> yolcular = new List<string>();
+        
+        public string kayit;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //yolcular[int.Parse(kayit)].Insert(1, textBox1.Text);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Form form1 = new Form1();
+    
+            //form1.Show();
+            this.Hide();
+        }
+    }
+}
